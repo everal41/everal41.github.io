6d9d6998187f67b2bb98670a56cf12e93c15a6a8
everal41 <152024838+everal41@users.noreply.github.com>
2023-12-24 18:04:55 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToMap() {
+    // Здесь добавьте код для перехода на страницу с интерактивной картой
+    window.location.href = 'Map/index.html';
+}
+
+function redirectToWiki() {
+    // Здесь добавьте код для перехода на страницу с википедией
+    window.location.href = 'Table/table.html';
+}
+
+// Добавьте дополнительные функции и обработчики событий по необходимости
